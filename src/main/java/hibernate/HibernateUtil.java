19de55a6b7d2920337c3d94a0026ffa8247bf946
dimonbugor <dimonbugor@gmail.com>
2018-08-21 21:38:16 +0300

    632

--- src/main/java/hibernate/HibernateUtil.java
+++ src/main/java/hibernate/HibernateUtil.java
@@ -1,17 +1,16 @@
 package hibernate;
 
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AnnotationConfiguration;
+
 
 
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
 
-    /*static {
+    static {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
@@ -20,18 +19,6 @@ public class HibernateUtil {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
-        }
-    }*/
-    static {
-        try {
-            StandardServiceRegistry standardRegistry
-                    = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-            Metadata metaData
-                    = new MetadataSources(standardRegistry).getMetadataBuilder().build();
-            sessionFactory = metaData.getSessionFactoryBuilder().build();
-        } catch (Throwable th) {
-            System.err.println("Enitial SessionFactory creation failed" + th);
-            throw new ExceptionInInitializerError(th);
         }
     }
 
